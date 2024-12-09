8a962e67794aa44f20664f96af9f37512dd62094
Mykolas A. Bamberg <m.a.bamberg.dev@proton.me>
2024-12-09 23:03:12 +0100

    added license

--- mandelbrot.d
+++ mandelbrot.d
@@ -1,3 +1,14 @@
+/*
+
+Copyright (C) 2024 Mykolas Bamberg <m.a.bamberg.dev@proton.me>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+*/
+
 import std.stdio;
 import std.format;
 import std.range;
